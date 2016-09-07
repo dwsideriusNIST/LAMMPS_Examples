357413d1f5391b6a410ae5341d9c30e7c1821ae2
Daniel W. Siderius <daniel.siderius@nist.gov>
2016-09-07 15:25:47 -0400

    Modified LJ_example to include analysis and renamed potential energy variable.

--- LJ_example/example.sh
+++ LJ_example/example.sh
@@ -2,5 +2,6 @@
 
 set echo
 
-ln -s ../LJ_initial_cfgs/in.nvt.dens_0.0400 ./
+ln -s ../LJ_initial_cfgs/in.nvt.dens_0.4000 ./
 mpirun -np 8 lmp_mpi -in NVT.startfromrestart -var rho 0.4000 -var temp 1.50
+../analysis/block_analysis.py -f ave.dens_0.4000.out -b 5 -m 100000
