c2dd2bccdfc58a1fa2a275c12cbe794b959e35cb
Daniel W. Siderius <daniel.siderius@nist.gov>
2017-07-20 16:54:19 -0400

    Updated SPC/E Example simulation to be faster

--- SPCE_example/example.sh
+++ SPCE_example/example.sh
@@ -4,4 +4,4 @@ set echo
 
 ln -s ../SPCE_initial_cfgs/spce_N1500_config.dens_1000kgm3.lammps ./
 mpirun -np 8 lmp_mpi -in in.spce -var infile spce_N1500_config.dens_1000kgm3.lammps -var temp 300.0
-../analysis/block_analysis.py -f ave.dens_1000kgm3.out -b 5 -m 100000
+../analysis/block_analysis.py -f ave.dens_1000kgm3.out -b 5 -m 10000
